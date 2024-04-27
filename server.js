b7e18692ea5dee2ee0f667957b3ceed934698b8a
Amina Mami <amina.mami@esprit.tn>
2024-04-27 02:14:27 +0100

    jenkinsfile

--- server.js
+++ server.js
@@ -89,16 +89,10 @@ wss.on("connection", (ws) => {
   });
 });
 
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => console.log("DB Connected"))
-//   .catch((err) => console.log(err));
-
-mongoose.connect("mongodb://root:example@localhost:27017/metamatch", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  authSource: "admin", // Specify the authentication database
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(bodyParser.json());
